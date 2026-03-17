f38263ad89996e44b7dfdf3b6cc3adc3777aca58
Klvin <klvinurban@gmail.com>
2026-03-17 01:56:36 -0500

    actualizacion

--- middleware.ts
+++ middleware.ts
@@ -29,7 +29,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: { path?: string } }[]) {
           cookiesToSet.forEach(({ name, value, options }) =>
             response.cookies.set(name, value, options)
           )
